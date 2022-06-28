8b3ce125f5864e3fccd4df6a05ac10aa346d5623
SDeYKan <abaran2004@gmail.com>
2022-06-28 16:49:09 +0200

    New GUI with basic menu, no functionality yet.

--- hotkeys.py
+++ hotkeys.py
@@ -1,16 +1,44 @@
-
-import time
+#IMPORTS
+#import time
 import tkinter as tk
-import keyboard
-# Variables
+from tkinter import *
+#import keyboard
+# VARIABLES
 settings = {"1": [0, "StopOnSpace"], "2": [0, "StopOnDelete"]}
-# Setup
+# SETUP
 settingsfile = open("settings.txt", "r").readlines()
 index = 0
 for setting in settings:
   index += 1
   settings[setting][0] = settingsfile[index][:-1]
   index += 1
-
-# Loop
-#TODO
+# SETTINGS WINDOW
+def createsettings():
+  new = Toplevel(root)
+  new.title("Settings")
+  newcanvas = tk.Canvas(new, width="280", height="600", bg="#FFFFFF")
+  newcanvas.grid(columnspan=3)
+# MAIN USER INTERFACE
+root = tk.Tk()
+root.title("Shrtct")
+# MAIN CANVAS
+canvas = tk.Canvas(root, width="760", height="400", bg="#FFFFFF")
+canvas.grid(columnspan=3)
+# TOP MENU
+menu = tk.Menu(root) # Create a main menu object
+root.config(menu=menu)
+# FILE
+filemenu = tk.Menu(menu) # Create a menu child for "menu", it will be one of the options
+menu.add_cascade(label="File", menu=filemenu) # Make "filemenu" an option on main menu, it'll appear as "File"
+filemenu.add_command(label="Exit", command=root.quit) # Add command "Exit" that will show when you click on "File" menu
+# EDIT
+editmenu = tk.Menu(menu) # Another menu child
+menu.add_cascade(label="Edit", menu=editmenu)
+editmenu.add_command(label="New Shortcut", command="")
+editmenu.add_command(label="Edit Shortcut", command="")
+editmenu.add_command(label="Remove Shortcut", command="")
+editmenu.add_separator()
+editmenu.add_command(label="Settings", command=createsettings)
+editmenu.add_command(label="Open Source Folder", command="")
+# MAINLOOP
+root.mainloop()
