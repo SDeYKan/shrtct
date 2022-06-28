65fda761c4a6a4163762b2efa57a4ad1f92ad027
SDeYKan <abaran2004@gmail.com>
2022-06-28 14:21:43 +0200

    Updated python program

--- hotkeys.py
+++ hotkeys.py
@@ -2,31 +2,15 @@
 import time
 import tkinter as tk
 import keyboard
-import mouse
+# Variables
+settings = {"1": [0, "StopOnSpace"], "2": [0, "StopOnDelete"]}
 # Setup
-event = 0
-history = []
-last = ""
-stoponmouse = False
-stoponkeys = False
-
-def remove(li: list, times: int, pos = "left") -> list:
-  if len(li) >= times:
-    if pos == "left":
-      for i in range(times):
-        li = li[1:]
-    elif pos == "right" or pos == "r":
-      for i in range(times):
-        li = li[:len(li) - 1]
-    else: print("Please use 'right' or 'r'")
-  else: print("List was not shortened")
-  return li
-  
-  return history
+settingsfile = open("settings.txt", "r").readlines()
+index = 0
+for setting in settings:
+  index += 1
+  settings[setting][0] = settingsfile[index][:-1]
+  index += 1
 
 # Loop
-while True:
-  # Section Listen
-  history.append(keyboard.read_key())
-  # End Section Listen
-  print(history)
+#TODO
