f074b28d61fe16c42f2f3ce902d20797b778de2d
SDeYKan <abaran2004@gmail.com>
2022-06-30 17:30:42 +0200

    Beta v1.0

--- eztk.py
+++ eztk.py
@@ -4,51 +4,93 @@ from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 
 class eztk:
+    # MAIN WINDOW
+    class place():
+        def place(self, object, x, y, side, p):
+            # Place is the default method for an easy xy coords object placements
+            # Because it can take longer than pack() or grid() it can be unconfortable for some
+            # If you don't want to use it, you can either just overwrite it by using other method in the next line
+            # You can add a p"pack" argument to change it for pack()
+            # You can use the side="" argument with pack()
+            # Grid needs too many arguments. You can still manually write it in a new line under the declaration!
+            if p == "place":
+                object.place(x=x, y=y)
+            elif p == "pack":
+                object.pack(side=side)
+            else:
+                print('Invalid placement argument. Leave black for "place", write "grid" or "pack" to change placement methods.')
+                return 2
     class mkwindow():
         def __init__(self, title="tk", size="300x300"):
             self.root = Tk()
             self.root.title(title)
             self.root.geometry(size)
-    class mklabel():
-        def __init__(self, parent, text="Sample", x=0, y=0):
+    # PLAIN TEXT
+    class mklabel(place):
+        def __init__(self, parent, text="Sample", x=0, y=0, side="", p="place"):
             self.label = tk.Label(parent.root, text=text)
-            self.label.place(x=x, y=y)
-    class mkentry():
-        def __init__(self, parent, x=0, y=0):
+            self.place(self.label, x, y, side, p)
+    # SMALL INPUT BOX
+    class mkentry(place):
+        def __init__(self, parent, x=0, y=0, side="", p="place"):
             self.variable = StringVar()
             self.entry = tk.Entry(parent.root, textvariable=self.variable)
-            self.entry.place(x=x, y=y)
+            self.place(self.entry, x, y, side, p)
         def getvar(self):
             return self.variable.get()
-    class mkcheck():
-        def __init__(self, parent, text="Sample", x=0, y=0):
+    # SIMPLE BUTTON
+    class mkbutton(place):
+        def __init__(self, parent, text="", width=15, height=1, command="", x=0, y=0, side="", p="place"):
+            self.button = tk.Button(parent.root, text=text, width=width, height=height, command=command)
+            self.place(self.button, x, y, side, p)
+    # CHECKBOX
+    class mkcheck(place):
+        def __init__(self, parent, text="Sample", x=0, y=0, side="", p="place"):
             self.variable = IntVar()
             self.checkbutton = tk.Checkbutton(parent.root, text=text, variable=self.variable)
-            self.checkbutton.place(x=x, y=y)
+            self.place(self.checkbutton, x, y, side, p)
         def getvar(self):
             return self.variable.get()
-    class mkradio():
-        def __init__(self, parent, text="Sample", value=1, x=0, y=0):
-            return
-            # Todo: This won't work like this. Radio buttons are intended to be multiple options for a variable in common.
-            #self.variable = IntVar()
-            #self.radiobutton = tk.Radiobutton(parent.root, text=text, variable=self.variable)
-            #self.radiobutton.place(x=x, y=y)
+    # LIST OF RADIO BUTTONS
+    class mkradiobuttons(place):
+        def __init__(self):
+            self.variable = IntVar()
+            self.lastposition = 0
+            self.buttons = {}
+        def addbutton(self, parent, number_of_buttons=1, names=["example"], x=0, y=0, side="", p="place", clearx=0, cleary=20):
+            list(names)
+            if number_of_buttons == len(names) and number_of_buttons >= 1:
+                for i in range(number_of_buttons):
+                    self.buttons[names[i]] = tk.Radiobutton(parent.root, text=names[i], variable=self.variable, value=self.lastposition + 1)
+                    self.place(self.buttons[names[i]], x+clearx*i, y+cleary*i, side, p)
+                    self.lastposition += 1
+            else:
+                print("The number of buttons does not equal the number of names you have given.")
+                return 1
         def getvar(self):
-            #return self.variable.get()
-            return
-    class mktextbox():
-        def __init__(self, parent, x=0, y=0, width=50, height=50):
+            return self.variable.get()
+    # LIST
+    class mklistbox(place):
+        def __init__(self, parent, width=80, height=150, x=0, y=0, side="", p="place"):
+            self.listbox = tk.Listbox(parent.root, width=width, height=height)
+            self.place(self.listbox, x, y, side, p)
+        def mkentry(self, position, text="Sample"):
+            self.listbox.insert(position, text)
+    # BIG INPUT BOX
+    class mktextbox(place):
+        def __init__(self, parent, x=0, y=0, width=50, height=50, side="", p="place"):
             self.box = Text(parent.root, width=width, height=height)
-            self.box.place(x=x, y=y)
+            self.place(self.box, x, y, side, p)
         def getvar(self):
             return self.box.get("1.0","end-1c")
-    class mkscrolledtextbox():
-        def __init__(self, parent, x=0, y=0, width=50, height=50):
+    # BIG INPUT BOX WITH SCROLL BAR
+    class mkscrolledtextbox(place):
+        def __init__(self, parent, x=0, y=0, width=50, height=50, side="", p="place"):
             self.box = ScrolledText(parent.root, width=width, height=height)
-            self.box.place(x=x, y=y)
+            self.place(self.box, x, y, side, p)
         def getvar(self):
             return self.box.get("1.0","end-1c")
+    # TOP MENU
     class topmenu():
         class mktopmenu():
             def __init__(self, parent):
@@ -65,18 +107,15 @@ class eztk:
         class mkseparator():
             def __init__(self, master_menu, submenu):
                 master_menu.submenus[submenu].add_separator()
-    class mkbutton():
-        def __init__(self, parent, text="", width=15, height=1, command="", x=0, y=0):
-            self.button = tk.Button(parent.root, text=text, width=width, height=height, command=command)
-            self.button.place(x=x, y=y)
-    class mklistbox():
-        def __init__(self, parent, width=80, height=150, x=0, y=0):
-            self.listbox = tk.Listbox(parent.root, width=width, height=height)
-            self.listbox.place(x=x, y=y)
-        def mkentry(self, position, text="Sample"):
-            self.listbox.insert(position, text)
+
+def stuff(button):
+    print(button.getvar())
+# Temporary
 def main():
     main = eztk.mkwindow(title="Main2")
+    button = eztk.mkbutton(main, "Print", command=lambda: stuff(label), p="pack", side="right")
+    label = eztk.mkradiobuttons()
+    label.addbutton(main, number_of_buttons=5, names=["hola", "adios", "hasta luego", "aur avoir", "nos vemos"])
     main.root.mainloop()
 if __name__ == "__main__":
     main()
