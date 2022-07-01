32b7049091a494b2b292a8588583b867d60cbe4a
SDeYKan <abaran2004@gmail.com>
2022-07-01 11:23:59 +0200

    Beta v1.1.1 (More improvements)

--- eztk.py
+++ eztk.py
@@ -2,6 +2,9 @@
 import tkinter as tk
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
+from turtle import right
+
+# todo: add treeview, add separator, add labelframe, add notebook, add messagebox(if useful)
 
 class place():
     def place(self, object, x, y, side):
@@ -16,10 +19,21 @@ class place():
             object.pack(side=side)
 # MAIN WINDOW
 class mkwindow():
-    def __init__(self, title="tk", size="300x300"):
+    def __init__(self, title="tk", size="300x300", iconpath="", height_resizable="False", width_resizable="False", close_function=None):
         self.root = Tk()
         self.root.title(title)
         self.root.geometry(size)
+        self.root.resizable(width_resizable, height_resizable)
+        if close_function:
+            self.root.protocol("WM_DELETE_WINDOW", close_function)
+        # Icon path is not relative to current path
+        self.root.iconbitmap(default=iconpath)
+    def run(self):
+        self.root.mainloop()
+    def update(self):
+        # Must be declared in a while(True) loop together with other code you want to repeat
+        # Although it is a possibility, it's relatively not good practice, as there is usually no need to run things in background
+        self.root.update()
 # TOP LEVEL WINDOW
 class mktoplevel():
     def __init__(self, title="tk", size="300x300"):
@@ -29,27 +43,27 @@ class mktoplevel():
 # PLAIN TEXT
 class mklabel(place):
     def __init__(self, parent, text="Sample", x=0, y=0, side=""):
-        self.label = tk.Label(parent.root, text=text)
-        self.place(self.label, x, y, side)
+        self.object = tk.Label(parent.root, text=text)
+        self.place(self.object, x, y, side)
 # SMALL INPUT BOX
 class mkentry(place):
     def __init__(self, parent, x=0, y=0, side=""):
         self.variable = StringVar()
-        self.entry = tk.Entry(parent.root, textvariable=self.variable)
-        self.place(self.entry, x, y, side)
+        self.object = tk.Entry(parent.root, textvariable=self.variable)
+        self.place(self.object, x, y, side)
     def getvar(self):
         return self.variable.get()
 # SIMPLE BUTTON
 class mkbutton(place):
     def __init__(self, parent, text="", width=15, height=1, command="", x=0, y=0, side=""):
-        self.button = tk.Button(parent.root, text=text, width=width, height=height, command=command)
-        self.place(self.button, x, y, side)
+        self.object = tk.Button(parent.root, text=text, width=width, height=height, command=command)
+        self.place(self.object, x, y, side)
 # CHECKBOX
 class mkcheck(place):
     def __init__(self, parent, text="Sample", x=0, y=0, side=""):
         self.variable = IntVar()
-        self.checkbutton = tk.Checkbutton(parent.root, text=text, variable=self.variable)
-        self.place(self.checkbutton, x, y, side)
+        self.object = tk.Checkbutton(parent.root, text=text, variable=self.variable)
+        self.place(self.object, x, y, side)
     def getvar(self):
         return self.variable.get()
 # LIST OF RADIO BUTTONS
@@ -58,39 +72,54 @@ class mkradiobuttons(place):
         self.variable = IntVar()
         self.lastposition = 0
         self.buttons = {}
-    def addbutton(self, parent, number_of_buttons=1, names=["example"], x=0, y=0, side="", clearx=0, cleary=20):
+    def addbuttons(self, parent, names=["example"], x=0, y=2, side="", clearx=0, cleary=20, background="#FFFFFF", activebackground="#FFFFFF"):
         list(names)
-        if number_of_buttons == len(names) and number_of_buttons >= 1:
-            for i in range(number_of_buttons):
-                self.buttons[names[i]] = tk.Radiobutton(parent.root, text=names[i], variable=self.variable, value=self.lastposition + 1)
-                self.place(self.buttons[names[i]], x+clearx*i, y+cleary*i, side)
-                self.lastposition += 1
-        else:
-            print("The number of buttons does not equal the number of names you have given.")
-            return 1
+        for i in range(len(names)):
+            self.buttons[names[i]] = tk.Radiobutton(parent.root, text=names[i], variable=self.variable, value=self.lastposition, bg=background, activebackground=activebackground)
+            self.place(self.buttons[names[i]], x+clearx*i, y+cleary*i, side=side)
+            self.lastposition += 1
     def getvar(self):
         return self.variable.get()
 # LIST
 class mklistbox(place):
-    def __init__(self, parent, width=80, height=150, x=0, y=0, side=""):
-        self.listbox = tk.Listbox(parent.root, width=width, height=height)
-        self.place(self.listbox, x, y, side)
-    def mkentry(self, position, text="Sample"):
-        self.listbox.insert(position, text)
+    def __init__(self, parent, width=80, height=150, x=0, y=0, side="", background="#FFFFFF"):
+        self.object = tk.Listbox(parent.root, width=width, height=height, bg=background)
+        self.object.bind("<<ListboxSelect>>", lambda x: self.getvar())
+        self.place(self.object, x, y, side)
+        self.lastposition = 0
+        self.selection = int
+    def addentry(self, names=["Sample"], start_position=-1):
+        if start_position < -1:
+            print("Wrong index!")
+            return 1
+        if start_position >= self.lastposition or start_position == -1:
+            for i in range(len(names)):
+                self.object.insert(self.lastposition, names[i])
+                self.lastposition += 1
+        else:
+            for i in range(len(names)):
+                if start_position < self.lastposition:
+                    self.object.delete(start_position)
+                    self.object.insert(start_position, names[i])
+                    start_position += 1
+                if start_position >= self.lastposition:
+                    self.lastposition += 1
+    def getvar(self):
+        self.selection = self.object.curselection()[0]
 # BIG INPUT BOX
 class mktextbox(place):
     def __init__(self, parent, x=0, y=0, width=50, height=50, side=""):
-        self.box = Text(parent.root, width=width, height=height)
-        self.place(self.box, x, y, side)
+        self.object = Text(parent.root, width=width, height=height)
+        self.place(self.object, x, y, side)
     def getvar(self):
-        return self.box.get("1.0","end-1c")
+        return self.object.get("1.0","end-1c")
 # BIG INPUT BOX WITH SCROLL BAR
 class mkscrolledtextbox(place):
     def __init__(self, parent, x=0, y=0, width=50, height=50, side=""):
-        self.box = ScrolledText(parent.root, width=width, height=height)
-        self.place(self.box, x, y, side)
+        self.object = ScrolledText(parent.root, width=width, height=height)
+        self.place(self.object, x, y, side)
     def getvar(self):
-        return self.box.get("1.0","end-1c")
+        return self.object.get("1.0","end-1c")
 # TOP MENU
 class topmenu():
     class mktopmenu():
@@ -108,9 +137,3 @@ class topmenu():
     class mkseparator():
         def __init__(self, master_menu, submenu):
             master_menu.submenus[submenu].add_separator()
-# GUI START
-def run_forever(program):
-    program.root.mainloop()
-def run_multitask(program):
-    # Must be declared in a while() loop that repeats forever or until something is triggered
-    program.root.update()
